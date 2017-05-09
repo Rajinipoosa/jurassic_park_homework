72b6807fb76cee5842c24f33456269329c2aeb64
Rajini <rajinipoosa@gmail.com>
2017-05-09 21:41:36 +0100

    wrote the function to display all the dinosaurs which satisfy the condition

--- park.js
+++ park.js
@@ -9,7 +9,31 @@ Park.prototype = {
   },
   addDinosaur: function (dinosaur){
     this.enclosure.push(dinosaur);
+  },
+
+  removeDinosaur: function (type) {
+    for (var closure of this.enclosure) {
+      if (closure.type === type) {
+        var index = this.enclosure.indexOf(closure);
+        this.enclosure.splice(index, 1);
+      }
+      
+
+      }
+    },
+  
+  getAllOffSpringDinosaur: function(offspring){
+    var num = [];
+    for (var closure of this.enclosure) {
+      if (closure.offspring = offspring) {
+        num.push(closure.type);
+        return num.length;
+        
+       
+        
   }
+}
+}
 
 };
 
--- specs/park_spec.js
+++ specs/park_spec.js
@@ -29,6 +29,20 @@ describe('A Park', function () {
    park.addDinosaur(dinosaur1);
    assert.strictEqual(1, park.enclosure.length);
  });
+ it('should be able to remove an dinosaur from enclosure', function () {
+   park.addDinosaur(dinosaur1);
+   park.addDinosaur(dinosaur2);
+   park.removeDinosaur('Tyrannosaurus');
+   assert.strictEqual(1, park.enclosure.length);
+ });
+ it('should able to get all dinosaur with offspring greater than 2', function() {
+  park.addDinosaur(dinosaur1);
+  park.addDinosaur(dinosaur2);
+  park.addDinosaur(dinosaur3);
+  var allDino = park.getAllOffSpringDinosaur(2);
+
+  assert.strictEqual(1,allDino);
+ })
 
   });
 
